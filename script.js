114e7d7cad671ea9b04ca270f6a6121e7f4f9044
Sky Adams <skyadams@sbunified.org>
2025-05-19 09:51:18 -0700

    Add names and URLs

--- script.js
+++ script.js
@@ -1,12 +1,75 @@
+var names = [
+  "Mason",
+  "Jacob"
+  "Samantha"
+  "Michelle"
+  "Carlos"
+  "Alice"
+  "Nicholas"
+  "Hollister"
+  "Rudy and Hollister"
+];
+
+var urls = [
+  "https://codehs.com/sandbox/id/car-animation-sS5Cy9",
+  "https://codehs.com/sandbox/id/keyboard-interaction-bbPUF5",
+  "https://codehs.com/sandbox/id/chatbot1-hsPsNY",
+  "https://codehs.com/sandbox/michellerstheprogamer/keyboard-interaction",
+  "https://codehs.com/sandbox/id/shark-animation-nHn2M4",
+  "https://codehs.com/sandbox/id/keyboard-interaction-3VnHmX",
+  "https://codehs.com/sandbox/id/function-ZfmyEh",
+  "https://codehs.com/sandbox/id/rocket-animation-oassVQ",
+  "https://codehs.com/sandbox/id/group-project-mxEuP7?collaborate=-OIq-Nq2ycbddsAnaeqR or https://group-project-13246376.codehs.me/index.html"
+];
+
+var gridWidth = 4;
+var gridHtml = "";
+var idx = 0;
+
+makeGrid();
+
 function generateCard(names, imageName, altText, url)
 {
-  return '<div class="card text-center"> <div class="card-body"> <h5 class="card-title">'
-    + names  
+    return '<div class="card text-center"> <div class="card-body"> <h5 class="card-title">'
+    + names
     + '</h5> <p class="card-text"> <a href="'
-    + url 
-    + '" target="_blank" rel="noopener noreferrer"> <img src="images/' 
-    + imageName 
+    + url
+    + '" target="_blank" rel="noopener noreferrer"> <img src="images/'
+    + imageName
     + '" alt="'
     + altText
     + '" class="screenshots"> </a> </p> </div> </div>';
+}
+
+function makeCard() {
+    if (idx >= names.length) return;
+    var name = names[idx];
+    var file = name.replaceAll(" ","").replaceAll(",","").replace("and","");
+    var card = generateCard(name, file + ".png", "platformer game", file + "/index.html");
+    gridHtml += card; //not elegant, but it works
+    
+    idx++;
+}
+
+
+function makeGrid() {
+    
+    for (var i = 0; i < names.length; i++) {
+        if (i % gridWidth == 0) {
+            gridHtml += '<div class="row">';
+        }
+        gridHtml += '<div class="col-md-3 p-3">';
+        makeCard();
+        gridHtml += '</div>';
+        if (i % gridWidth == (gridWidth - 1)) {
+            gridHtml += '</div>';
+        }
+    }
+    
+    if (names.length % gridWidth != 0) {
+        gridHtml += '</div>';
+    }
+    
+    document.getElementById("grid").innerHTML += gridHtml;
+    
 }
